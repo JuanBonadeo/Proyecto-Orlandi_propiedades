37907ba1df34407a53e22b3c79c81ac55b742e2b
Juan Cruz <juancruzbonadeo04@gmail.com>
2024-05-29 14:35:01 -0300

    rurales

--- src/components/ComprarRurales/ComprarRurales.jsx
+++ src/components/ComprarRurales/ComprarRurales.jsx
@@ -6,7 +6,7 @@ import ProductsContainer from '../ProductsContainer/ProductsContainer'
 const ComprarRurales = () => {
   return (
     <>
-      <Hero4 title="Propiedades Rurales" img="https://firebasestorage.googleapis.com/v0/b/orlandi-propiedades.appspot.com/o/heroComprarRurales.jpg?alt=media&token=95d1a25b-b605-4d26-a788-bfd7539ae498" />
+      <Hero4 title="Propiedades Rurales" img="https://firebasestorage.googleapis.com/v0/b/orlandi-propiedades.appspot.com/o/heroComprarRurales.jpg?alt=media&token=7459e447-ed2d-4f41-b457-442cd6c21807" />
       <div className="comprarContainer">
         <ProductsContainer categoria='campos' />
       </div>
