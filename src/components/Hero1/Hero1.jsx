5eb8a32a3a6ca1685f018bc3757745c2bc4f8231
Juan Cruz <juancruzbonadeo04@gmail.com>
2024-05-29 14:27:45 -0300

    sape

--- src/components/Hero1/Hero1.jsx
+++ src/components/Hero1/Hero1.jsx
@@ -21,7 +21,7 @@ const Hero1 = () => {
         <Carousel.Item className='Hero1Container'>
           <img
             className="imgHero1"
-            src='https://firebasestorage.googleapis.com/v0/b/orlandi-propiedades.appspot.com/o/1192096%201%20(1)%20(1)%20(1)%20(1).png?alt=media&token=9362964d-382a-4001-9a43-6916d6e1ee3f'
+            src='https://firebasestorage.googleapis.com/v0/b/orlandi-propiedades.appspot.com/o/foto1.png?alt=media&token=555e2122-cf6a-4a38-9c81-a5a71c9985ec'
             alt="First-slide"
           />
           
